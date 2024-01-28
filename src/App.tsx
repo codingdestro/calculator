7dbd3f9ed31ba8f1276970537f4d34b9e438687f
mohd anas <localmail.office@gmail.com>
2024-01-28 20:15:01 +0530

    add - keys functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import KeyBox from "./components/KeyBox";
 
 const keys = [
   "C",
-  "D",
-  "%",
+  "00",
+  "←",
   "/",
   "7",
   "8",
@@ -26,14 +26,74 @@ const keys = [
 
 function App() {
   const [input, setInput] = useState<string>("");
+  const [data, setData] = useState<string[]>([""]);
+  const [left, setLeft] = useState("");
+  const [right, setRight] = useState("");
+
+  const isOperator = (key: string): boolean => {
+    if (key === "+" || key === "x" || key === "/" || key === "-") return true;
+    return false;
+  };
+
+  const addSegment = (segment: string) => {
+    setRight("");
+    setLeft((prevState) => prevState + segment);
+    setData((prevState) =>
+      prevState[0] ? [...prevState, segment] : [segment]
+    );
+  };
+  const popSegment = () => {
+    if (data) {
+      let lastItem = data.pop() || "";
+      if (isOperator(lastItem)) {
+        lastItem = data.pop() || "";
+      }
+      setRight(lastItem);
+      setLeft(() => {
+        let x = data.reduce(
+          (previousValue: string, currentValue: string) =>
+            previousValue + currentValue,
+          ""
+        );
+
+        return x;
+      });
+    }
+  };
 
   const getInput = (key: string) => {
-    setInput((prevState) => prevState + key);
+    if (key === "C") {
+      setRight("");
+      setLeft("");
+    } else if (key === "=") {
+      if (right && !isOperator(input[input.length - 1])) {
+        console.log("calculate the value", input);
+      }
+    } else if (key === ".") {
+      if (right) {
+        setRight((prevState) =>
+          prevState.includes(".") ? prevState : prevState + key
+        );
+      }
+    } else if (isOperator(key)) {
+      if (right) {
+        addSegment(right);
+        addSegment(key);
+      }
+    } else if (key === "←") {
+      if (right) {
+        setRight((prevState) => prevState.slice(0, -1));
+      } else {
+        popSegment();
+      }
+    } else {
+      setRight((prevState) => prevState + key);
+    }
   };
 
   useEffect(() => {
-    console.log(input);
-  }, [input]);
+    setInput(left + right);
+  }, [right, left]);
 
   return (
     <div className="body">
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,10 +1,10 @@
-interface Props{
-    text:string
+interface Props {
+  text: string;
 }
-const Display = ({text}:Props) => {
+const Display = ({ text }: Props) => {
   return (
     <div className="display-box">
-      <div className="main">{text}</div>
+      <div className={`main ${!text&&"fade"}`}>{!text || text == "0"?"0":text}</div>
     </div>
   );
 };
