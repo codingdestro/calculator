6535c2b240a8a6a227066fc8267a684be5db42ab
codingdester <localmail.office@gmail.com>
2024-07-15 12:45:01 +0530

    update - convertor style

--- src/components/converter/Select.tsx
+++ src/components/converter/Select.tsx
@@ -14,41 +14,46 @@ type Props = {
 const Select = ({ options, node, setNode, setNode2 }: Props) => {
   const convertOnChange = (from: string, val: string) => {
     setNode2((prevState) => {
-      let newValue = convert(
+      const newValue = convert(
         parseInt(from),
         parseInt(prevState.key),
         parseFloat(val),
-        options
+        options,
       );
       return { key: prevState.key.toString(), val: newValue.toString() };
     });
   };
 
   const selectOption = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    let key = e.target.value;
+    const key = e.target.value;
     convertOnChange(key, node.val);
     setNode((prevState) => ({ key: key, val: prevState.val }));
   };
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    let val = e.target.value;
+    const val = e.target.value;
     convertOnChange(node.key, val);
+
     setNode((prevState) => ({ key: prevState.key, val }));
   };
 
   return (
     <>
-      <div className="select-box">
-        <select onChange={(e) => selectOption(e)}>
+      <div className="flex flex-col items-start gap-2 my-2">
+        <select
+          onChange={(e) => selectOption(e)}
+          className="px-5 py-1 rounded-lg cursor-pointer "
+        >
           {options.map((value, idx: number) => {
             return (
-              <option className={`option `} value={idx} key={idx}>
+              <option className={``} value={idx} key={idx}>
                 {value?.key}
               </option>
             );
           })}
         </select>
         <input
+          className="border rounded-lg w-full px-2 "
           type="number"
           value={node.val}
           onChange={onChangeHandler}
--- src/components/converter/index.tsx
+++ src/components/converter/index.tsx
@@ -1,24 +1,24 @@
-import "../../styles/select.scss";
-import "../../styles/converter.scss";
 import SelectBox from "./SelectBox";
 import { Options } from "../../service/data";
 import { useState } from "react";
-const index = () => {
+const Home = () => {
   const [key, setKey] = useState(0);
 
   return (
-    <div className="converter">
-      <h3>Unit Converter</h3>
-      <div className="unit-box">
+    <div className="border rounded-md shadow-md p-5 w-[300px]">
+      <h1 className="text-xl border-b mb-2  font-bold capitalize text-center">
+        Unit Converter
+      </h1>
+      <div className="border-b flex gap-5 justify-between items-center px-2 pb-2 mb-5 rounded-lg ">
         {Options.map((value, idx: number) => {
           return (
-            <div
-              className={`unit ${idx === key ? "show" : "hide"}`}
+            <button
+              className={`px-1 rounded-md transition-all ${idx === key && "bg-violet-100"}`}
               key={idx}
               onClick={() => setKey(idx)}
             >
               {value.name}
-            </div>
+            </button>
           );
         })}
       </div>
@@ -27,4 +27,4 @@ const index = () => {
   );
 };
 
-export default index;
+export default Home;
