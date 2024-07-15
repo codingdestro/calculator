032a4a3f83614075c0e72723a241c997ea32e955
codingdester <localmail.office@gmail.com>
2024-07-15 12:09:09 +0530

    update - toggle bar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Calculator from "./components/calculator";
+import ToggleBar from "./components/ToggleBar";
+import UnitConverter from "./components/converter";
+
+const Components = ["calculator", "unit converter"];
 
 const App = () => {
   return (
     <>
       <div>
-        <Calculator />
+        <ToggleBar elements={Components}>
+          <Calculator />
+          <UnitConverter />
+        </ToggleBar>
       </div>
     </>
   );
--- src/components/calculator/Key.tsx
+++ src/components/calculator/Key.tsx
@@ -6,7 +6,7 @@ const Key = ({ name, onPress }: KeyProps) => {
   return (
     <button
       onClick={() => onPress(name)}
-      className="border flex-1 px-3 py-1 rounded-md "
+      className="border flex-1 px-3 py-1 rounded-md  shadow-md  "
     >
       {name}
     </button>
