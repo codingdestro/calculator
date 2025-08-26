3cd80137f416bd912e1da07fc7345b3cb071bd7b
codingdestro <codingdestro@gmail.com>
2025-08-26 22:13:02 +0530

    add: made ToggleBar responsive and scrollable

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import Calculator from "./components/calculator";
 import ToggleBar from "./components/ToggleBar";
 import UnitConverter from "./components/converter";
 import AgeCalc from "./components/age_calc";
+import Emi from "./components/emi";
 
-const Components = ["calculator", "unit converter", "age calc"];
+const Components = ["calculator","emi", "unit converter", "age calc"];
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-100 to-slate-200 py-4 sm:py-8 px-4">
+    <div className="min-h-screen bg-gradient-to-br from-slate-100 to-slate-200 py-4 sm:py-8 px-4 ">
       <div className="container mx-auto max-w-4xl">
         <ToggleBar elements={Components}>
           <Calculator />
+          <Emi/>
           <UnitConverter />
           <AgeCalc />
         </ToggleBar>
